3b267b15566b2b331934a33d6cfec92fc5440741
devilserial <74173775+devilserial@users.noreply.github.com>
2020-11-12 19:44:17 +0800

    增加Fragment

---
+++ app/src/main/java/com/dev/words/AddFragment.java
@@ -0,0 +1,104 @@
+package com.dev.words;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddFragment newInstance(String param1, String param2) {
+        AddFragment fragment = new AddFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+    private Button buttonSubmit;
+    private EditText editEnglish,editChinese;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        buttonSubmit = requireActivity().findViewById(R.id.buttonSubmit);
+        editEnglish = requireActivity().findViewById(R.id.editEnglish);
+        editChinese = requireActivity().findViewById(R.id.editChinese);
+        buttonSubmit.setEnabled(false);
+        editEnglish.requestFocus();
+        InputMethodManager inputMethodManager = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.showSoftInput(editEnglish,0);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String english = editEnglish.getText().toString().trim();
+                String chinese = editChinese.getText().toString().trim();
+                buttonSubmit.setEnabled(!english.isEmpty() && !chinese.isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+    }
+}
--- app/src/main/java/com/dev/words/MainActivity.java
+++ app/src/main/java/com/dev/words/MainActivity.java
@@ -3,6 +3,9 @@ package com.dev.words;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,109 +19,26 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonInsert, buttonClear;
 
-    WordViewModel wordViewModel;
-    MyAdapter myAdapter1, myAdapter2;
-    RecyclerView recyclerView;
-    Switch aSwitch;
-
+    private NavController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wordViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(WordViewModel.class);
-        buttonInsert = findViewById(R.id.buttonInsert);
-
-        buttonClear = findViewById(R.id.buttonClear);
-
-        recyclerView = findViewById(R.id.recylerView);
-        aSwitch = findViewById(R.id.switch1);
-        myAdapter1 = new MyAdapter(false,wordViewModel);
-        myAdapter2 = new MyAdapter(true,wordViewModel);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(myAdapter1);
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-
-                    recyclerView.setAdapter(myAdapter2);
-                } else {
-
-                    recyclerView.setAdapter(myAdapter1);
-                }
-            }
-        });
-
-
-
-
-        //观察数据库的内容变化
-        wordViewModel.getListLiveDataWords().observe(this, new Observer<List<Word>>() {
-            @Override
-            public void onChanged(List<Word> words) {
-                int temp = myAdapter1.getItemCount();
-                myAdapter1.setAllWords(words);
-                myAdapter2.setAllWords(words);
-                if(temp != words.size()){
-                    myAdapter1.notifyDataSetChanged();
-                    myAdapter2.notifyDataSetChanged();
-                }
-            }
-        });
-
-
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String english[] = {
-                        "Hello",
-                        "World",
-                        "Google",
-                        "Japan",
-                        "Chronometer",
-                        "Repository",
-                        "Android",
-                        "Apple",
-                        "USA",
-                        "NEW",
-                        "Recycler",
-                        "Project"
-                };
-                String chineseMeaing[] = {
-                        "你好",
-                        "世界",
-                        "谷歌",
-                        "日本",
-                        "计时器",
-                        "仓库",
-                        "安卓",
-                        "苹果",
-                        "美国",
-                        "新世界",
-                        "回收站",
-                        "项目"
-                };
-                int i = 0;
-                for (String string : english) {
-                    wordViewModel.insertWords(new Word(english[i], chineseMeaing[i]));
-                    i++;
-                }
-
-            }
-        });
-        buttonClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                wordViewModel.deleteAllWords();
-
-            }
-        });
-
-
+        controller = Navigation.findNavController(findViewById(R.id.fragment));
+        NavigationUI.setupActionBarWithNavController(this,controller);
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        controller.navigateUp();
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        controller.navigateUp();
+        return super.onSupportNavigateUp();
+    }
 }
